c09ddd46b8327f5b0c4b20b58c54b8bf88aa9cd1
fokie <ilnarfokie@gmail.com>
2023-06-13 14:21:20 +0300

    add 2 themes, and toggle logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import "./styles/index.scss"
 import { Link } from "react-router-dom"
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async"
 import { MainPageAsync } from "./pages/MainPage/MainPage.async"
-import { Suspense } from "react"
-
+import { Suspense, useContext, useState } from "react"
+import { useTheme } from "./theme/useTheme"
 
 export const App = () => {
+
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
 
