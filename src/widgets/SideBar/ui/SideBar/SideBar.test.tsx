2275e74d91a2c241085246abb2b43f5a13b3c4aa
fokie <ilnarfokie@gmail.com>
2023-07-29 21:12:22 +0300

    fixed componentRender

--- src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -1,17 +1,17 @@
 import { fireEvent, screen } from "@testing-library/react"
 import { SideBar } from "./SideBar"
 import { 
-	renderWithTranslation 
-} from "shared/lib/tests/renderWithTranslation/renderWithTranslation"
+	componentRender 
+} from "shared/config/tests/componentRender/componentRender"
 
 describe("SideBar", () => {
 	test("test render", () => {
-		renderWithTranslation(<SideBar />)
+		componentRender(<SideBar />)
 		expect(screen.getByTestId("sidebar")).toBeInTheDocument()
 	})
 
 	test("test toggle", () => {
-		renderWithTranslation(<SideBar />)
+		componentRender(<SideBar />)
 		const toggleBtn = screen.getByTestId("sidebar-toggle")
 		expect(screen.getByTestId("sidebar")).toBeInTheDocument()
 		fireEvent.click(toggleBtn)
